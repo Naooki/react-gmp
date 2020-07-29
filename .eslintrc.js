31fe7f06ab8f515e83370bec748a9ec0ca29d109
Yahor Sheinikau <yahor_sheinikau@epam.com>
2020-07-29 23:34:27 +0300

    Adds jest config. Adds sample unit-test.

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     ecmaVersion: 2018,
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: './tsconfig.eslint.json',
   },
   plugins: ['react', '@typescript-eslint', 'jest'],
   rules: {},
