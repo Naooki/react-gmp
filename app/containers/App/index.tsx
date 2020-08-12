1c6306c83f65b697a959db88d1645bd722f182d5
Yahor Sheinikau <yahor_sheinikau@epam.com>
2020-08-12 16:41:48 +0300

    Adds styled-components.

--- app/containers/App/index.tsx
+++ app/containers/App/index.tsx
@@ -4,14 +4,18 @@ import { Switch, Route } from 'react-router-dom';
 import Login from 'containers/Login';
 import Home from 'containers/Home';
 import NotFoundPage from 'containers/NotFoundPage';
+import GlobalStyle from 'global-styles';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/login" component={Login} />
-      <Route path="/" component={Home} />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route path="/" component={Home} />
+        <Route component={NotFoundPage} />
+      </Switch>
+      <GlobalStyle />
+    </>
   );
 }
 
