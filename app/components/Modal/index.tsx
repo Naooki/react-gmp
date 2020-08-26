798cf24337335c4de55c5446678139848eb93232
Yahor Sheinikau <yahor_sheinikau@epam.com>
2020-08-27 02:21:29 +0300

    Refactors modal to use context, ref and portal.

--- app/components/Modal/index.tsx
+++ app/components/Modal/index.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import ReactDOM from 'react-dom';
 import styled from 'styles/styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+const Container = styled.div`
+  position: relative;
+  z-index: 0;
+`;
 
 const Overlay = styled.div`
   position: fixed;
@@ -13,7 +19,7 @@ const Overlay = styled.div`
   backdrop-filter: blur(1rem);
 `;
 
-const ModalWrapper = styled.section`
+const Dialog = styled.section`
   position: fixed;
   max-height: 80%;
   width: 60%;
@@ -47,24 +53,53 @@ const ModalMain = styled.main`
   overflow-y: scroll;
 `;
 
+const mockDiv = document.createElement('div');
+mockDiv.classList.add('modal-container');
+const Context = React.createContext(mockDiv);
+
+export function ModalProvider({ children }: { children: React.ReactNode }) {
+  const modalRef = React.useRef<HTMLDivElement>(null);
+  const [context, setContext] = React.useState<HTMLDivElement>(mockDiv);
+
+  // make sure re-render is triggered after initial
+  // render so that modalRef exists
+  React.useEffect(() => {
+    if (modalRef.current) {
+      setContext(modalRef.current);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Context.Provider value={context}>{children}</Context.Provider>
+      <div ref={modalRef} />
+    </Container>
+  );
+}
+
 interface Props {
   children: React.ReactNode;
   onClose: () => void;
 }
 
 const Modal = (props: Props) => {
-  return (
-    <Overlay>
-      <ModalWrapper>
-        <ModalHeader>
-          <ModalCloseBtn onClick={props.onClose}>
-            <FontAwesomeIcon icon={faTimes} />
-          </ModalCloseBtn>
-        </ModalHeader>
-        <ModalMain>{props.children}</ModalMain>
-      </ModalWrapper>
-    </Overlay>
-  );
+  const modalNode = React.useContext(Context);
+
+  return modalNode
+    ? ReactDOM.createPortal(
+        <Overlay>
+          <Dialog>
+            <ModalHeader>
+              <ModalCloseBtn onClick={props.onClose}>
+                <FontAwesomeIcon icon={faTimes} />
+              </ModalCloseBtn>
+            </ModalHeader>
+            <ModalMain>{props.children}</ModalMain>
+          </Dialog>
+        </Overlay>,
+        modalNode,
+      )
+    : null;
 };
 
 export default Modal;
